1af1291d88a94a93f10eb344f8d146c44e03aa40
Justin <zallambo@gmail.com>
2021-01-02 15:53:32 -0500

    Forgot to commit this

---
+++ src/notation_constructors.rs
@@ -0,0 +1,33 @@
+use super::notation::{Notation, RepeatInner};
+
+pub fn nl() -> Notation {
+    Notation::Newline
+}
+
+pub fn child(i: usize) -> Notation {
+    Notation::Child(i)
+}
+
+pub fn lit(s: &str) -> Notation {
+    Notation::Literal(s.to_string())
+}
+
+pub fn flat(n: Notation) -> Notation {
+    Notation::Flat(Box::new(n))
+}
+
+pub fn left() -> Notation {
+    Notation::Left
+}
+
+pub fn right() -> Notation {
+    Notation::Right
+}
+
+pub fn surrounded() -> Notation {
+    Notation::Surrounded
+}
+
+pub fn repeat(repeat: RepeatInner) -> Notation {
+    Notation::Repeat(Box::new(repeat))
+}
