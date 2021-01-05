54e774b1ad0f72cd3c680991df974c3aa2593af6
Justin <zallambo@gmail.com>
2021-01-05 17:33:58 -0500

    docs

--- src/pretty_printing/pretty_doc.rs
+++ src/pretty_printing/pretty_doc.rs
@@ -1,13 +1,17 @@
 use crate::notation::Notation;
 
-/// A "document" that supports the necessary methods to be pretty-printed.
+/// A node in a "document" that supports the necessary methods to be pretty-printed.
 pub trait PrettyDoc: Sized {
     type Id: Eq + Copy;
 
+    /// An id that uniquely identifies this node.
     fn id(&self) -> Self::Id;
+    /// The node's notation.
     fn notation(&self) -> &Notation;
+    /// Get the contents of this document node. It may contain text, or children, but not both.
     fn contents(&self) -> PrettyDocContents<Self>;
 
+    /// Get this node's text, or panic.
     fn unwrap_text(&self) -> &str {
         match self.contents() {
             PrettyDocContents::Text(text) => text,
@@ -15,6 +19,7 @@ pub trait PrettyDoc: Sized {
         }
     }
 
+    /// Get this node's number of children, or `None` if it contains text instead.
     fn num_children(&self) -> Option<usize> {
         match self.contents() {
             PrettyDocContents::Text(_) => None,
@@ -22,6 +27,7 @@ pub trait PrettyDoc: Sized {
         }
     }
 
+    /// Get this node's i'th child, or panic.
     fn unwrap_child(&self, i: usize) -> &Self {
         match self.contents() {
             PrettyDocContents::Text(_) => panic!("PrettyDoc: expected children"),
@@ -32,6 +38,7 @@ pub trait PrettyDoc: Sized {
     }
 }
 
+/// The contents of a node in a document. It may contain text, or children, but not both.
 #[derive(Debug)]
 pub enum PrettyDocContents<'d, D: PrettyDoc> {
     Text(&'d str),
