483352e19a42c98179520bb1c07457dc730800ee
Justin <zallambo@gmail.com>
2021-09-07 15:49:54 -0400

    (End of WIP.) Clean up scratchwork.

--- src/pretty_printing/mod.rs
+++ src/pretty_printing/mod.rs
@@ -2,9 +2,7 @@ mod notation_ref;
 mod oracle;
 mod pretty_doc;
 mod pretty_print;
-//mod pretty_print_2;
 
 pub use oracle::oracular_pretty_print;
 pub use pretty_doc::PrettyDoc;
 pub use pretty_print::{pretty_print, pretty_print_to_string, LineContents};
-//pub use pretty_print_2::{pretty_print, pretty_print_to_string, LineContents};
